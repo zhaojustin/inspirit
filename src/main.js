4fbcc366f736c88d2e570c186f47e4fce7285109
Justin Zhao <justin@justinzhao.net>
2020-09-16 17:16:10 -0400

    added clock

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-//set background
+//clock---------------------------
+var timeFormat = true;
+var showNoon = null;
+function updateClock() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  //12 hour time format
+  if (!timeFormat) {
+    var noon = "AM";
+    if (h == 0) h = 12;
+    if (h > 12) {
+      h = h - 12;
+      noon = "PM";
+    }
+  }
+  m = m < 10 ? "0" + m : m;
+  document.getElementById("clock").innerHTML = h + ":" + m;
+
+  //clock tick
+  setTimeout(updateClock, 1000);
+}
+
+//initiate clock
+updateClock();
